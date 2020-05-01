0007de4f7a1a4051446bc12cad825b2b71f76342
jmunox <jmunoza@live.com>
2020-05-01 12:57:57 +0200

    Adding support for @bulma 0.8.2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # 🚀 Parcel UI
 
-[![Build Status](https://travis-ci.org/ngduc/parcelui.svg?branch=master)](https://travis-ci.org/ngduc/parcelui)
+<!--[![Build Status](https://travis-ci.org/ngduc/parcelui.svg?branch=master)](https://travis-ci.org/ngduc/parcelui)-->
 
-Parcel + Typescript + React/Preact + Router + CSS Modules + SASS + Jest + Api-Now
+Parcel + Typescript + React/Preact + Router + CSS Modules + SASS + Jest + Api-Now + Bulma
 
 And more:
 
@@ -12,13 +12,14 @@ And more:
 - Component unit testing with Jest & Enzyme.
 - Making Ajax requests using Whatwg-fetch Req with Mock APIs (json-server).
 - Mock API server with [api-now](https://github.com/ngduc/api-now).
+- Responsive and modular CSS using [Bulma CSS framework](https://bulma.io/).
 
 ### 🔧 Installation
 
 Clone this project:
 
 ```
-git clone https://github.com/ngduc/parcelui.git your-app
+git clone https://github.com/jmunox/parcelui.git your-app
 cd your-app
 rm -rf .git
 yarn
@@ -43,6 +44,10 @@ yarn serve            serve static built dir "./dist" using api-now (or any web 
 
 - SCSS is recommended, but optional. You can also use ".css" files normally.
 
+#### Bulma
+- Bulma is a free, open source CSS framework based on Flexbox https://bulma.io/
+
+
 ### Others
 
 Recommend:
@@ -50,3 +55,11 @@ Recommend:
 - VSCode & prettier addon
 
 <img src="tools/dev.jpg">
+
+### Change log
+#### 2020-04-29 
+Clone from https://github.com/ngduc/parcelui [commit](https://github.com/ngduc/parcelui/commit/e54fa345659100d532ead7257749799ee6b94a2a)
+Updated dev-dependencies:
+- Added Bulma (^0.8.2),
+- Updated postcss-modules to ^1.5.0,
+- Updated sass to ^1.25.0
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,30 +2,39 @@ import * as React from 'react'
 import { view } from 'react-easy-state'
 import todoStore from 'stores/todoStore'
 
+import * as css from './Home.scss'
+
 export default view(() => {
   const { items, create, remove, fetchItems } = todoStore
   return (
-    <div>
-      <div>Home Page</div>
-
-      <button data-fetch onClick={fetchItems}>
-        Fetch API
+    <div className={css.container} >
+      <div className={css.title}>Home Page</div>
+      <div className={[css.mozColumns].join(' ')}>
+        <div className={[css.column, css.isOneFifth].join(' ')} >
+          <button className={[css.button, css.isFullwidth].join(' ')} data-fetch onClick={fetchItems}>
+            Fetch API
       </button>
-      <button data-add onClick={create}>
-        ＋ Add Item
+        </div>
+        <div className={[css.column, css.isOneFifth].join(' ')} >
+          <button className={[css.button, css.isFullwidth].join(' ')} data-add onClick={create}>
+            ＋ Add Item
       </button>
-
-      <ul>
-        {items &&
-          items.map((item, idx) => (
-            <li key={idx}>
-              {item.title}{' '}
-              <button data-remove onClick={() => remove(idx)}>
-                X
+        </div>
+      </div>
+      <div className={[css.mozColumns].join(' ')}>
+        <ul className={[css.column, css.isTwoFifths].join(' ')} >
+          {items &&
+            items.map((item, idx) => (
+              <li className={[css.media].join(' ')} key={idx}>
+                <span className={[css.mediaContent].join(' ')}>{item.title}{' '}</span>
+                <button 
+                    className={[css.delete, css.mediaRight].join(' ')} 
+                    data-remove onClick={() => remove(idx)}>
               </button>
-            </li>
-          ))}
-      </ul>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   )
 })
